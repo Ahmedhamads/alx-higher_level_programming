5e34437a474827909065c1b9114aac4c789923b7
AhmedHamads <ahmedhamads276@gmail.com>
2023-11-27 12:00:00 -0800

    new_pyton_repo

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - check if lists is cyclial
+ * @list: pointer to list to check
+ * Return: 1 if cycle, 0oherwise
+ */
+int check_cycle(listint_tt *list)
+{
+listint_tt *slow = list, *fast = list;
+while(fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if(slow == fast)
+return 1;
+}
+}
