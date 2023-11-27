08259cf083f87604755d7c7bb9d6cf9f0ab20532
AhmedHamads <ahmedhamads276@gmail.com>
2023-11-27 12:33:06 -0800

    hw

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,14 +6,15 @@
  * @list: pointer to list to check
  * Return: 1 if cycle, 0oherwise
  */
-int check_cycle(listint_tt *list)
+int check_cycle(listint_t *list)
 {
-listint_tt *slow = list, *fast = list;
+listint_t *slow = list, *fast = list;
 while(fast && fast->next)
 {
 slow = slow->next;
 fast = fast->next->next;
 if(slow == fast)
-return 1;
+return (1);
 }
+return(0);
 }
